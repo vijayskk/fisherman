d20b17197e2661fa33def8fd661f5ef1e3abb4d0
vijayskk <vijayskk007@gmail.com>
2025-08-02 10:57:13 +0530

    Thread hancler

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,80 @@
 #include "main.hpp"
 
+void help();
+void thread_handler(int start,int stop);
 
-void scan();
 
-
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-    int num_of_threads = thread::hardware_concurrency();
-    cout << "Available threads = " << num_of_threads << "\n";
-    scan();
-    for(auto i : open_ports){
-        cout<< i << "\n";
+    if (argc == 1 or (strcmp(argv[1], "-") == 0))
+    {
+        help();
+        return 1;
     }
+
+    int c, start, end;
+    while ((c = getopt(argc, argv, "supah")) != -1)
+    {
+        switch (c)
+        {
+        case 's':
+            start = 0;
+            end = 1023;
+            break;
+        case 'u':
+            start = 1024;
+            end = 49151;
+            break;
+        case 'p':
+            start = 49152;
+            end = 65535;
+            break;
+        case 'a':
+            start = 0;
+            end = 65535;
+            break;
+        case 'h':
+            help();
+            break;
+        default:
+            help();
+            break;
+        }
+        thread_handler(start,end); // TODO: Implement this
+    }
+
     return 0;
 }
 
-void scan(){
-    for (int i = 0; i < 65535; i++)
+
+void help(){
+    cout << "\t'-s' - Scan system ports\n"
+         << "\t'-u' - Scan user ports\n"
+         << "\t'-p' - Scan private ports\n"
+         << "\t'-a' - Scan all ports\n"
+         << "\t'-h' - Print this menu.\n";
+}
+
+void count_open_ports(int start, int end){
+    
+}
+
+void thread_handler(int start,int end){
+    int max_threads = thread::hardware_concurrency();
+    thread threadlist[max_threads];
+    int interval = (end - start + 1)/max_threads;
+    int thread_num;
+
+    for (thread_num = 0; thread_num < max_threads; thread_num++)
     {
-        int sockfd;
-        struct sockaddr_in address;
+        int rightbound = start+interval;
+        threadlist[thread_num] = thread(count_open_ports,start,rightbound);
+        start = rightbound + 1;
+    }
 
-        if (inet_pton(AF_INET,"127.0.0.1",&address.sin_addr) < 1) // Checks for ip can be binded or not
-        {
-            std::cerr << "Problem Loading the ip address!!\n";
-            exit(EXIT_FAILURE);
-        }
-        
-        memset(&address,0,sizeof(address));
-        address.sin_family = AF_INET; // IPv4
-        address.sin_addr.s_addr = inet_addr("127.0.0.1");
-        address.sin_port = htons(i);
-
-        if ((sockfd = socket(PF_INET,SOCK_STREAM,0)) < 0)
-        {
-            close(sockfd);
-            continue;
-        }
-        if (connect(sockfd,(struct sockaddr *)&address,sizeof(address)) == 0)
-        {
-            open_ports.push_back(i);
-        }
-        close(sockfd);
+    for(thread_num = 0 ; thread_num < max_threads ; thread_num ++){
+        threadlist[thread_num].join();
     }
     
+
 }
